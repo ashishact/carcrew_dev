adabbf6c4fba0c35b1985172e890efdca4ad86e5
Ashish Charan Tandi <act.jnv@gmail.com>
2016-07-26 12:45:52 +0530

    APIs using Generics, shortest of all

--- jsonapi/views.py
+++ jsonapi/views.py
@@ -6,6 +6,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from rest_framework import generics
+
 from jsonapi.models import Car, Product,  Garage
 from jsonapi.serializers import CarSerializers, ProductSerializers, GarageSerializers
 
@@ -25,52 +27,61 @@ def index(request):
 
 
 # Cars ######################################################
-class CarList(APIView):
-    """
-    List all cars, or create a new car.
-    """
-
-    def get(self, request, format=None):
-        cars = Car.objects.all()
-        serializer = CarSerializers(cars, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = CarSerializers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CarList(generics.ListCreateAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializers
 
 
-class CarDetail(APIView):
-    """
-    send details of a car
-    """
-    def get_object(self, pk):
-        try:
-            return Car.objects.get(pk=pk)
-        except Car.DoesNotExist:
-            raise Http404
+class CarDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializers
 
-    def get(self, request, pk, format=None):
-        car = self.get_object(pk)
-        serializer = CarSerializers(car)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        car = self.get_object(pk)
-        serializer = CarSerializers(car, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        car = self.get_object(pk)
-        car.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+# class CarList(APIView):
+#     """
+#     List all cars, or create a new car.
+#     """
+#
+#     def get(self, request, format=None):
+#         cars = Car.objects.all()
+#         serializer = CarSerializers(cars, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = CarSerializers(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class CarDetail(APIView):
+#     """
+#     send details of a car
+#     """
+#     def get_object(self, pk):
+#         try:
+#             return Car.objects.get(pk=pk)
+#         except Car.DoesNotExist:
+#             raise Http404
+#
+#     def get(self, request, pk, format=None):
+#         car = self.get_object(pk)
+#         serializer = CarSerializers(car)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         car = self.get_object(pk)
+#         serializer = CarSerializers(car, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         car = self.get_object(pk)
+#         car.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
 
 # Product ######################################################
 class ProductList(APIView):
